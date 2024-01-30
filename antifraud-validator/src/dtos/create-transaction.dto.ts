2939929aaffa1f62ca5fbe783f5c880b2ec53603
JuanCordoneda <juancordoneda@gmail.com>
2024-01-30 14:39:06 -0300

    Revert "implementation"
    This reverts commit 28ce6cfda7c62593aa7b8b043fdd22642cbe0bfc.

--- antifraud-validator/src/dtos/create-transaction.dto.ts
+++
@@ -1,7 +0,0 @@
-export class TransactionDto {
-  public readonly transactionExternalId!: string;
-  public readonly accountExternalIdDebit!: string;
-  public readonly accountExternalIdCredit!: string;
-  public readonly transferTypeId!: number;
-  public readonly value!: number;
-}
